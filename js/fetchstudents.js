d4260d6fa14e687c66cb3374ded8599132e09473
ERLMKea <erlm@kea.dk>
2022-09-15 09:45:01 +0200

    Done reading students

--- js/fetchstudents.js
+++ js/fetchstudents.js
@@ -7,14 +7,21 @@ const pbGetStudents = document.getElementById("pbGetStudent")
 function fetchStudents() {
     out("inside fetchstudents")
     const studentUrl = "http://localhost:8080/students"
-    return  fetch(studentUrl).then(response => response.json());
+    //return  fetch(studentUrl).then(response => response); //just returns the response object
+    return  fetch(studentUrl).then(response => response.json()); //returns the result of json()
+}
+
+function printStudent(student) {
+    out(student)
+    out("id=" + student.id)
+    out("name=" + student.name)
 }
 
 async function doFetchStudents(btn) {
     out("fetch students")
     let students = await fetchStudents();
-    out(students);
-
+    out(students); //students is an array
+    students.forEach(printStudent)
 }
 
 pbGetStudents.addEventListener('click', doFetchStudents)
