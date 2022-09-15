bb1f92a33bca0af86067c634c814ae24db83877a
ERLMKea <erlm@kea.dk>
2022-09-15 11:30:22 +0200

    Create table with photos now with image tag

--- js/createtable.js
+++ js/createtable.js
@@ -1,5 +1,5 @@
 out("im in create table")
-const marsUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?sol=100&page=1&api_key=cXrM4POFLGgBFlOOAbiEk3K0q2NfzynY0Ckydqap"
+const marsUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?sol=100&page=2&api_key=cXrM4POFLGgBFlOOAbiEk3K0q2NfzynY0Ckydqap"
 const pbGetPhotos = document.getElementById("pbGetPhotos")
 
 const tblPhotos = document.getElementById("tblMars")
@@ -22,19 +22,45 @@ function createTableHardCoded() {
 }
 
 function createTable(photo) {
+    if (!photo.id) return;
+
     let rowCount = tblPhotos.rows.length
     out("rowcount=" + rowCount)
     let row = tblPhotos.insertRow(rowCount)
+
     let cell1 = row.insertCell(0)
-    cell1.innerHTML= "1"
+    cell1.innerHTML= photo.id
+
     let cell2 = row.insertCell(1)
-    cell2.innerHTML= "Kurt"
+    cell2.innerHTML= photo.sol
 
     let cell3 = row.insertCell(2)
+    cell3.innerHTML= photo.rover.name
+
+    let cell4 = row.insertCell(3)
+    cell4.innerHTML= photo.camera.name
+
+    let cell5 = row.insertCell(4)
     let atag = document.createElement("a")
-    atag.setAttribute("href", "https://www.dr.dk/")
-    atag.innerText = "Danish Radio"
-    cell3.appendChild(atag)
+    atag.setAttribute("href", photo.img_src)
+    atag.innerText = photo.id
+    cell5.appendChild(atag)
+
+    //<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">
+
+    //const img1 = "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00100/ids/edr/browse/ncam/NLF_0100_0675828767_506ECM_N0040218NCAM00503_01_295J01_1200.jpg";
+
+    //let imghtml = '<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">'
+    //let imghtmlstart =
+
+    let cell6 = row.insertCell(5)
+    let img = document.createElement("img")
+    img.setAttribute("src", photo.img_src)
+    img.setAttribute("alt", photo.id)
+    img.setAttribute("width", 100)
+    img.setAttribute("height", 100)
+    cell6.appendChild(img)
+
 }
 
 
@@ -54,14 +80,20 @@ function printPhoto(photo) {
 }
 
 async function doFetchPhoto(btn) {
+    //out(btn.srcElement.id) //the id of the button that was pressed
     out("fetch photos")
     let photos = await fetchPhotos();
     out(photos); //students is an array
     let photoArr = photos.photos;
-    photoArr.forEach(printPhoto)
+    if (btn.srcElement.id == "pbGetPhotos") {
+        photoArr.forEach(printPhoto)
+    } else {
+        photoArr.forEach(createTable)
+    }
 }
 
 pbGetPhotos.addEventListener('click', doFetchPhoto)
+pbCreatTable.addEventListener('click', doFetchPhoto)
 
 
 
